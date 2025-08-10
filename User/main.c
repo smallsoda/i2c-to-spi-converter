77c5ada9e0b879fb6436c2f15b41536d58dd49a5
Dmitry Proshutinsky <sodaspace@yandex.ru>
2025-08-10 17:18:01 +0300

    main: add gpio input
    - add gpio input functionality
    - update commands
    
    Signed-off-by: Dmitry Proshutinsky <sodaspace@yandex.ru>

--- User/main.c
+++ User/main.c
@@ -1,25 +1,38 @@
 /**
  * @author Dmitry Proshutinsky (sodaspace@yandex.ru)
- * @brief I2C to SPI/GPO interface converter
+ * @brief I2C to SPI/GPIO interface converter
  * @note Connection diagram:
  *
- *           SWIO  o  o
- *       ┌──────┴──┴──┴──────┐
- *       │   SWIO  V  G      │
- *  SCK ─┤ PC5           PC4 ├─ MODE
- * MOSI ─┤ PC6           PC3 ├─ BUSY
- * MISO ─┤ PC7           PC2 ├─ SCL
- *  NSS ─┤ PD0           PC1 ├─ SDA
- * SWIO ─┤ PD1           PC0 ├─ ?
- * GPO1 ─┤ PD2           PA2 ├─ ?
- * GPO2 ─┤ PD3           PA1 ├─ ?
- * GPO3 ─┤ PD4           PD6 ├─ RX
- * NRST ─┤ PD7           PD5 ├─ TX
- *    o ─┤ V               V ├─ o
- *    o ─┤ G               G ├─ o
- *       │      VT1772       │
- *       │   CH32V003F4P6    │
- *       └───────────────────┘
+ *            SWIO  o  o
+ *        ┌──────┴──┴──┴──────┐
+ *        │   SWIO  V  G      │
+ *   SCK ─┤ PC5           PC4 ├─ MODE
+ *  MOSI ─┤ PC6           PC3 ├─ BUSY
+ *  MISO ─┤ PC7           PC2 ├─ SCL
+ *   NSS ─┤ PD0           PC1 ├─ SDA
+ *  SWIO ─┤ PD1           PC0 ├─ ?
+ * GPIO1 ─┤ PD2           PA2 ├─ ?
+ * GPIO2 ─┤ PD3           PA1 ├─ ?
+ * GPIO3 ─┤ PD4           PD6 ├─ RX
+ *  NRST ─┤ PD7           PD5 ├─ TX
+ *     o ─┤ V               V ├─ o
+ *     o ─┤ G               G ├─ o
+ *        │      VT1772       │
+ *        │   CH32V003F4P6    │
+ *        └───────────────────┘
+ * @note Commands:
+ * ┌────┬────┬────┬────┬────┬────┬────┬────┐
+ * │ b7 │ b6 │ b5 │ b4 │ b3 │ b2 │ b1 │ b0 │
+ * ├────┴────┴────┴────┼────┴────┴────┴────┤
+ * │      action       │      object       │
+ * ├───────────────────┼───────────────────┤
+ * │ 0 - GET           │ 0 - SPI           │
+ * │ 1 - RESET         │ 1 - GPIO1         │
+ * │ 2 - SET           │ 2 - GPIO2         │
+ * │ 3 - MODE          │ 3 - GPIO3         │
+ * │ 4 - BAUDRATE      │ 4 - RXC           │
+ * │                   │ 5 - TXC           │
+ * └───────────────────┴───────────────────┘
  */
 
 #include <string.h>
@@ -27,39 +40,62 @@
 #include "ch32v00x.h"
 #include "debug.h"
 
-#define VERSION "0.03"
-
-#define CMD_GET_RXC      0x11
-#define CMD_GET_TXC      0x12
-#define CMD_RESET_RXC    0x91
-#define CMD_RESET_TXC    0x92
-#define CMD_CTRL_GPO1    0xA1
-#define CMD_CTRL_GPO2    0xA2
-#define CMD_CTRL_GPO3    0xA3
-#define CMD_SPI_MODE     0xC1
-#define CMD_SPI_BAUDRATE 0xC2
-
-#define GPO1 1
-#define GPO2 2
-#define GPO3 3
-
-#define SPI_MODE_0 0 // CPOL = 0, CPHA = 0
-#define SPI_MODE_1 1 // CPOL = 0, CPHA = 1
-#define SPI_MODE_2 2 // CPOL = 1, CPHA = 0
-#define SPI_MODE_3 3 // CPOL = 1, CPHA = 1
-
-#define SPI_BAUDRATE_2   1
-#define SPI_BAUDRATE_4   2
-#define SPI_BAUDRATE_8   3
-#define SPI_BAUDRATE_16  4
-#define SPI_BAUDRATE_32  5
-#define SPI_BAUDRATE_64  6
-#define SPI_BAUDRATE_128 7
-#define SPI_BAUDRATE_256 8
+#define VERSION "0.04"
 
 #define I2C_ADDRESS 0x51
 
 #define MAP_SIZE 1024
+
+#define CMD_RXC_GET      0x04
+#define CMD_TXC_GET      0x05
+#define CMD_RXC_RESET    0x14
+#define CMD_TXC_RESET    0x15
+#define CMD_GPIO1_GET    0x01
+#define CMD_GPIO2_GET    0x02
+#define CMD_GPIO3_GET    0x03
+#define CMD_GPIO1_SET    0x21
+#define CMD_GPIO2_SET    0x22
+#define CMD_GPIO3_SET    0x23
+#define CMD_GPIO1_MODE   0x31
+#define CMD_GPIO2_MODE   0x32
+#define CMD_GPIO3_MODE   0x33
+#define CMD_SPI_MODE     0x30
+#define CMD_SPI_BAUDRATE 0x40
+
+enum gpion
+{
+    GPIO1,
+    GPIO2,
+    GPIO3
+};
+
+enum conf_gpio_mode
+{
+    GPIO_MODE_IN_FL  = 0x00,
+    GPIO_MODE_IN_PU  = 0x01,
+    GPIO_MODE_OUT_PP = 0x02,
+    GPIO_MODE_OUT_OD = 0x03
+};
+
+enum conf_spi_mode
+{
+    SPI_MODE_0 = 0x00, // CPOL = 0, CPHA = 0
+    SPI_MODE_1 = 0x01, // CPOL = 0, CPHA = 1
+    SPI_MODE_2 = 0x02, // CPOL = 1, CPHA = 0
+    SPI_MODE_3 = 0x03  // CPOL = 1, CPHA = 1
+};
+
+enum conf_spi_baudrate
+{
+    SPI_BAUDRATE_2   = 0x01,
+    SPI_BAUDRATE_4   = 0x02,
+    SPI_BAUDRATE_8   = 0x03,
+    SPI_BAUDRATE_16  = 0x04,
+    SPI_BAUDRATE_32  = 0x05,
+    SPI_BAUDRATE_64  = 0x06,
+    SPI_BAUDRATE_128 = 0x07,
+    SPI_BAUDRATE_256 = 0x08
+};
 
 uint8_t map[MAP_SIZE];
 volatile size_t len = 0;
@@ -118,74 +154,6 @@ static void init_i2c(void)
     I2C_Cmd(I2C1, ENABLE);
 }
 
-/* gpio mode initialization ***************************************************/
-/**
- * @note gpio mode states:
- * - HIGH - data transfer
- * - LOW - configuration
- */
-static void init_gpio_mode(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-
-    // PC4 - MODE
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-/* gpio busy initialization ***************************************************/
-/**
- * @note gpio busy states:
- * - HIGH - busy
- * - LOW - ready
- */
-static void init_gpio_busy(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-
-    // PC3 - BUSY
-    GPIO_ResetBits(GPIOC, GPIO_Pin_3);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-/* gpio user initialization ***************************************************/
-static void init_gpio_user(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-
-    // PD2 - GPO1
-    GPIO_ResetBits(GPIOD, GPIO_Pin_2);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    // PD3 - GPO2
-    GPIO_ResetBits(GPIOD, GPIO_Pin_3);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    // PD4 - GPO3
-    GPIO_ResetBits(GPIOD, GPIO_Pin_4);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-}
-
 /* spi initialization *********************************************************/
 static void init_spi(void)
 {
@@ -217,6 +185,7 @@ static void init_spi(void)
     // PC7 - MISO
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
     // SPI
@@ -239,14 +208,11 @@ static void init_spi(void)
 }
 
 /* spi update mode ************************************************************/
-static void update_spi_mode(uint8_t mode)
+static void update_spi_mode(enum conf_spi_mode mode)
 {
-    uint16_t reg, cpy;
+    uint16_t reg;
 
-    SPI_Cmd(SPI1, DISABLE);
     reg = SPI1->CTLR1;
-
-    cpy = reg;
     reg &= ~0x0003; // xxxx xxxx xxxx xx00
 
     switch (mode)
@@ -264,23 +230,20 @@ static void update_spi_mode(uint8_t mode)
         reg |= SPI_CPOL_High | SPI_CPHA_2Edge;
         break;
     default: // invalid mode
-        reg = cpy;
-        break;
+        return;
     }
 
+    SPI_Cmd(SPI1, DISABLE);
     SPI1->CTLR1 = reg;
-    SPI_Cmd(SPI1, ENABLE);
+    SPI_Cmd(SPI1, ENABLE); // ?
 }
 
 /* spi update mode ************************************************************/
-static void update_spi_baudrate(uint8_t baudrate)
+static void update_spi_baudrate(enum conf_spi_baudrate baudrate)
 {
-    uint16_t reg, cpy;
+    uint16_t reg;
 
-    SPI_Cmd(SPI1, DISABLE);
     reg = SPI1->CTLR1;
-
-    cpy = reg;
     reg &= ~0x0038; // xxxx xxxx xx00 0xxx
 
     switch (baudrate)
@@ -310,12 +273,12 @@ static void update_spi_baudrate(uint8_t baudrate)
         reg |= SPI_BaudRatePrescaler_256;
         break;
     default: // invalid baudrate
-        reg = cpy;
-        break;
+        return;
     }
 
+    SPI_Cmd(SPI1, DISABLE);
     SPI1->CTLR1 = reg;
-    SPI_Cmd(SPI1, ENABLE);
+    SPI_Cmd(SPI1, ENABLE); // ?
 }
 
 /* dma spi tx initialization **************************************************/
@@ -364,6 +327,120 @@ static void init_dma_spi_rx(void)
     NVIC_EnableIRQ(DMA1_Channel2_IRQn);
 }
 
+/* gpio mode initialization ***************************************************/
+/**
+ * @note gpio mode states:
+ * - HIGH - data transfer
+ * - LOW - configuration
+ */
+static void init_gpio_mode(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    // PC4 - MODE
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+/* gpio busy initialization ***************************************************/
+/**
+ * @note gpio busy states:
+ * - HIGH - busy
+ * - LOW - ready
+ */
+static void init_gpio_busy(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    // PC3 - BUSY
+    GPIO_ResetBits(GPIOC, GPIO_Pin_3);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+/* gpio user initialization ***************************************************/
+static void init_gpio_user(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+
+    // PD2 - GPIO1
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    // PD3 - GPIO2
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    // PD4 - GPIO3
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+/* gpio update mode ***********************************************************/
+static void update_gpio_mode(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin,
+    enum conf_gpio_mode mode)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+    switch (mode)
+    {
+    case GPIO_MODE_IN_FL:
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+        break;
+    case GPIO_MODE_IN_PU:
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+        break;
+    case GPIO_MODE_OUT_PP:
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+        break;
+    case GPIO_MODE_OUT_OD:
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+        break;
+    default: // invalid mode
+        return;
+    }
+
+    GPIO_Init(GPIOx, &GPIO_InitStructure);
+}
+
+/* gpio user update mode ******************************************************/
+static void update_gpio_user_mode(enum gpion num, enum conf_gpio_mode mode)
+{
+    switch (num)
+    {
+    case GPIO1:
+        update_gpio_mode(GPIOD, GPIO_Pin_2, mode);
+        break;
+    case GPIO2:
+        update_gpio_mode(GPIOD, GPIO_Pin_3, mode);
+        break;
+    case GPIO3:
+        update_gpio_mode(GPIOD, GPIO_Pin_4, mode);
+        break;
+    default: // invalid num
+        break;
+    }
+}
+
 /* mode state *****************************************************************/
 inline static uint8_t is_conf_mode(void)
 {
@@ -376,7 +453,7 @@ inline static void busy_active(void)
     GPIO_SetBits(GPIOC, GPIO_Pin_3);
 }
 
-/* busy inactive ****************************************************************/
+/* busy inactive **************************************************************/
 inline static void busy_inactive(void)
 {
     GPIO_ResetBits(GPIOC, GPIO_Pin_3);
@@ -394,21 +471,39 @@ inline static void spi_cs_inactive(void)
     GPIO_SetBits(GPIOD, GPIO_Pin_0);
 }
 
-/* gpio user ************************************************************/
-inline static void gpio_user_set(uint8_t gpo, uint8_t val)
+/* gpio user get **************************************************************/
+inline static uint8_t gpio_user_get(enum gpion num)
 {
-    switch (gpo)
+    switch (num)
     {
-    case GPO1:
+    case GPIO1:
+        return GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_2);
+    case GPIO2:
+        return GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_3);
+    case GPIO3:
+        return GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_4);
+    default: // invalid num
+        break;
+    }
+
+    return 0;
+}
+
+/* gpio user set **************************************************************/
+inline static void gpio_user_set(enum gpion num, uint8_t val)
+{
+    switch (num)
+    {
+    case GPIO1:
         GPIO_WriteBit(GPIOD, GPIO_Pin_2, val ? Bit_SET : Bit_RESET);
         break;
-    case GPO2:
+    case GPIO2:
         GPIO_WriteBit(GPIOD, GPIO_Pin_3, val ? Bit_SET : Bit_RESET);
         break;
-    case GPO3:
+    case GPIO3:
         GPIO_WriteBit(GPIOD, GPIO_Pin_4, val ? Bit_SET : Bit_RESET);
         break;
-    default: // invalid gpo
+    default: // invalid num
         break;
     }
 }
@@ -431,10 +526,16 @@ static void conf_rd(void)
 {
     uint8_t cmd = map[0];
 
-    if (cmd == CMD_GET_RXC)
+    if (cmd == CMD_RXC_GET)
         memcpy(map, (uint8_t *) &rxc, sizeof(rxc));
-    else if (cmd == CMD_GET_TXC)
+    else if (cmd == CMD_TXC_GET)
         memcpy(map, (uint8_t *) &txc, sizeof(txc));
+    else if (cmd == CMD_GPIO1_GET)
+        map[0] = gpio_user_get(GPIO1);
+    else if (cmd == CMD_GPIO2_GET)
+        map[0] = gpio_user_get(GPIO2);
+    else if (cmd == CMD_GPIO3_GET)
+        map[0] = gpio_user_get(GPIO3);
 }
 
 /* configuration mode - write conf ********************************************/
@@ -443,16 +544,22 @@ static void conf_wr(void)
     uint8_t cmd = map[0];
     uint8_t val = map[1];
 
-    if (cmd == CMD_RESET_RXC)
+    if (cmd == CMD_RXC_RESET)
         rxc = 0;
-    else if (cmd == CMD_RESET_TXC)
+    else if (cmd == CMD_TXC_RESET)
         txc = 0;
-    else if (cmd == CMD_CTRL_GPO1)
-        gpio_user_set(GPO1, val);
-    else if (cmd == CMD_CTRL_GPO2)
-        gpio_user_set(GPO2, val);
-    else if (cmd == CMD_CTRL_GPO3)
-        gpio_user_set(GPO3, val);
+    else if (cmd == CMD_GPIO1_SET)
+        gpio_user_set(GPIO1, val);
+    else if (cmd == CMD_GPIO2_SET)
+        gpio_user_set(GPIO2, val);
+    else if (cmd == CMD_GPIO3_SET)
+        gpio_user_set(GPIO3, val);
+    else if (cmd == CMD_GPIO1_MODE)
+        update_gpio_user_mode(GPIO1, val);
+    else if (cmd == CMD_GPIO2_MODE)
+        update_gpio_user_mode(GPIO2, val);
+    else if (cmd == CMD_GPIO3_MODE)
+        update_gpio_user_mode(GPIO3, val);
     else if (cmd == CMD_SPI_MODE)
         update_spi_mode(val);
     else if (cmd == CMD_SPI_BAUDRATE)
